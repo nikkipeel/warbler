35dd96b14ecb31ed76c1f3dc9e16e2d48df7c98e
nikkipeel <npeel3497@gmail.com>
2020-03-15 17:52:19 -0400

    Create auth.js

---
+++ warbler-client/src/store/actions/auth.js
@@ -0,0 +1,26 @@
+export function logout() {
+    return dispatch => {
+        localStorage.clear();
+        setAuthorizationToken(false);
+        dispatch(setCurrentUser({}));
+    };
+}
+
+export function authUser(type, userData) {
+    return dispatch => {
+        return new Promise((resolve, reject) => {
+            return apiCall("post", `/api/auth/${type}`, userData).then(
+                ({ token, ...user }) => {
+                    localStorage.setItem("jwtToken", token);
+                    setAuthorizationToken(token);
+                    dispatch(setCurrentUser(user));
+                    dispatch(removeError());
+                    resolve();
+                })
+                .catch(err => {
+                    dispatch(addError(err.message));
+                    reject();
+            });
+        });
+    };
+}
