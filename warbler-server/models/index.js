726b42f02ceea24974a77a023232aad9ae5c0fa3
nikkipeel <npeel3497@gmail.com>
2020-03-15 17:40:44 -0400

    Create index.js

---
+++ warbler-server/models/index.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+mongoose.set("debug", true);
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/warbler", {
+    keepAlive: true,
+    //useMongoClient: true,
+    //useNewUrlParser: true
+});
+
+module.exports.User = require("./user");
+module.exports.Message = require("./message");
